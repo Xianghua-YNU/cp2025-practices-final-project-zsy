4dd14a8fdf5e462d363bd69c2b4657d937807789
zhusiyuv <1477117691@qq.com>
2025-06-24 16:58:55 +0800

    Update numerical_methods.py

--- 2_Code/numerical_methods.py
+++ 2_Code/numerical_methods.py
@@ -19,3 +19,28 @@ def solve_ode(initial_conditions, time_points):
     return solution
 
 # 可以添加更多数值方法，如积分、求根、矩阵运算等
+def Gauss_Seidel(Nx, Ny, l_2):
+    T = np.zeros([Nx + 2, Ny + 2])
+    dx, dy = 1 / Nx, 1 / Ny
+    L = []
+    iteration = []
+
+    L2 = 1
+    counter = 0
+    while L2 > l_2:
+
+        boundary_condition(T, Nx, Ny)
+        T_0 = T.copy()
+
+        for m in range(1,Nx+1):
+            for n in range(1, Ny+1):
+                T[m,n] = 0.5*(dy**2/((dy**2+dx**2))*(T_0[m,n+1]+T[m,n-1])+dx**2/((dy**2+dx**2))*(T[m-1,n]+T_0[m+1,n]))
+
+        Error = np.abs(T_0[1:Nx+1, 1:Ny+1] - T[1:Nx+1, 1:Ny+1])
+        L2 = np.sqrt(np.mean(Error**2))
+        L.append(L2)
+        counter += 1
+        iteration.append(counter)
+    L = np.array(L)
+    iteration = np.array(iteration)
+    return T[1:Nx+1, 1:Ny+1], iteration, L
